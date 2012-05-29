18a4ef6f53d689c6f5a747e3e8b203af0d698791
hayfield <hayfield@gmx.com>
2012-05-29 19:56:58 +0100

    Get tweets displaying nicely

--- src/js/presentationFeedback.js
+++ src/js/presentationFeedback.js
@@ -1,8 +1,40 @@
 $(function() {
+    var lastTweetID = Number.MIN_VALUE;
+    var tweetCount = 0;
+    
     $('#goButton').click(function(){
-        
         if( $('#URLInput').attr('value').length > 1 ){
             $('#presentationIframe').attr('src', $('#URLInput').attr('value'));
         }
+        
+        fetchTweets();
     });
+    
+    function fetchTweets(){
+        var tweep = $('#twitterInput').attr('value');
+        
+        console.log(tweep, 'hello', lastTweetID);
+        
+        $.getJSON('http://search.twitter.com/search.json?since_id=' + lastTweetID + '&q=%40' + tweep + '&callback=?', function(data){
+            lastTweetID = data.max_id;
+            console.log(data);
+            var results = data.results;
+            
+            for( var i = results.length - 1; i >= 0; i-- ){
+                var tweetColor = tweetCount % 2 == 0 ? 'odd' : 'even';
+                
+                $('#twitterInfo')
+                .prepend(
+                    $(document.createElement('div'))
+                    .addClass('tweet')
+                    .addClass(tweetColor)
+                    .text(results[i].text)
+                );
+                
+                tweetCount++;
+            }
+            
+            window.setTimeout( fetchTweets, 5000 );
+        });
+    };
 });
